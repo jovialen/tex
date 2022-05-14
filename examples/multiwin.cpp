2b35d676062b47a117dff170d537078921965e62
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-05-14 17:37:14 +0200

    add multi-window example

---
+++ examples/multiwin.cpp
@@ -0,0 +1,16 @@
+#include <tex.hpp>
+
+int main(int argc, char *argv[])
+{
+	tex::init();
+
+	tex::world world1("1");
+	tex::world world2("2");
+
+	while (tex::update(world1) && tex::update(world2))
+	{
+		
+	}
+
+	tex::quit();
+}
