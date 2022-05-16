21b8811ee55c258d25df5c4830e7802b358697d8
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-05-16 15:14:18 +0200

    add more windows to multiwin example bc fun

--- examples/multiwin.cpp
+++ examples/multiwin.cpp
@@ -2,11 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-	tex::world world1("1");
-	tex::world world2("2");
+	tex::world worlds[4];
 
-	while (tex::update(world1) && tex::update(world2))
+	bool running = true;
+	while (running)
 	{
-		
+		for (int i = 0; i < 4; i++)
+		{
+			running &= tex::update(worlds[i]);
+		}
 	}
 }
